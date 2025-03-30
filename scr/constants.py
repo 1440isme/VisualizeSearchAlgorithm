64866b29aaf6cab2b66f9490226c6498e5999b0d
MaxTrann <tranlequocdai2005@gmail.com>
2025-03-30 14:42:02 +0700

    Update note  constrants

--- scr/constants.py
+++ scr/constants.py
@@ -4,7 +4,7 @@ import pygame
 pygame.font.init()
 pygame.display.init()
 
-# Colors
+# Màu sắc
 BLACK = (0, 0, 0)
 DARK = (11, 53, 71)
 GREEN = (26, 188, 157)
@@ -18,15 +18,16 @@ BLUE_2 = (81, 145, 228)
 DARK_BLUE_2 = (44, 67, 208)
 PURPLE = (17, 104, 217)
 
-# Window Dimensions
+# Kích thước cửa sổ
 WINDOW_INFO = pygame.display.Info()
 SCREEN_WIDTH, SCREEN_HEIGHT = WINDOW_INFO.current_w, WINDOW_INFO.current_h
 WIDTH = 1280 if SCREEN_WIDTH >= 1280 else SCREEN_WIDTH - 150
 HEIGHT = 900 if SCREEN_HEIGHT >= 900 else SCREEN_HEIGHT - 150
-HEADER_HEIGHT = 200
+HEADER_HEIGHT = 200 # phần header UI phía trên mê cung.
 
-# Maze
-CELL_SIZE = 26
+# Thiết lập kích thước ô - (CELL_SIZE)
+
+CELL_SIZE = 26 # mỗi ô mặc định 26px * 26px 
 if len(sys.argv) > 1:
     arg = sys.argv[1]
 
@@ -47,28 +48,31 @@ if len(sys.argv) > 1:
         print("USAGE: python3 run.pyw [ --cell-size:<int> ]")
         exit(1)
 
+# Tính toán lại kích thước mê cung 
+# Tính phần dư của chiều ngang
 REMAINDER_W = WIDTH % CELL_SIZE
 if REMAINDER_W == 0:
     REMAINDER_W = CELL_SIZE
 
+# Tính phần cư của chiều cao (ko tính header)
 REMAINDER_H = (HEIGHT - HEADER_HEIGHT) % CELL_SIZE
 if REMAINDER_H == 0:
     REMAINDER_H = CELL_SIZE
 
-MAZE_WIDTH = WIDTH - REMAINDER_W
-MAZE_HEIGHT = HEIGHT - HEADER_HEIGHT - REMAINDER_H
+MAZE_WIDTH = WIDTH - REMAINDER_W # chiều rộng mê cung (chia hết cho CELL_SIZE)
+MAZE_HEIGHT = HEIGHT - HEADER_HEIGHT - REMAINDER_H #	chiều cao mê cung (bỏ phần header, chia hết CELL_SIZE)
 
-# Framerate
+# Thiết lập FPS - tốc độ chạy chương trình
 FPS = 60
 CLOCK = pygame.time.Clock()
 
-# Images and fonts
+# Load hình ảnh và chữ
 WEIGHT = pygame.image.load("assets/images/weight.png")
 START = pygame.image.load("assets/images/triangle.png")
 GOAL = pygame.image.load("assets/images/circle.png")
 FONT_14 = pygame.font.Font("assets/fonts/Montserrat-Regular.ttf", 14)
 FONT_18 = pygame.font.Font("assets/fonts/Montserrat-Regular.ttf", 18)
 
-# Animations
+# Thông số animation
 MIN_SIZE = 0.3 * CELL_SIZE
 MAX_SIZE = 1.2 * CELL_SIZE
